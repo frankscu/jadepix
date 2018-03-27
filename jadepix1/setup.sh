f9b6f0549e86890ced4323ae96f9e7ded99ddc20
Xin Shi <shixin@ihep.ac.cn>
2018-03-27 09:59:47 +0800

    use master allpix and cluseter prototype

--- jadepix1/setup.sh
+++ jadepix1/setup.sh
@@ -4,7 +4,24 @@ Geant4Bin=/opt/allpix/geant4.10.04/install/bin
 source ${Geant4Bin}/geant4.sh 
 source /opt/allpix/root/install/bin/thisroot.sh
 
-# Add allpix-squared v1.1.1 
-export ALLPIX=/opt/allpix/v1.1.1
+if [[ $# -eq 0 ]]; then
+    printf "\nUsing default setup.\n"
+else
+    option=$1
+fi
+
+
+case $option in
+    master) echo "Using master allpix2 ..."
+    export ALLPIX=/home/shixin/allpix-squared
+    ;;
+
+    *) echo "Setting up allpix-squared v1.1.1 ..." 
+    export ALLPIX=/opt/allpix/v1.1.1
+    ;;     
+
+esac
+
 export PATH=$PATH:$ALLPIX/bin:$ALLPIX/bin/tcad_dfise_converter
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ALLPIX/lib
+
